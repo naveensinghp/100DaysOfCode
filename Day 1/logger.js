42499eb478df09c5464a2f27af9246b49944ce88
Naveen Singh <30367189+naveensinghp@users.noreply.github.com>
2021-04-01 19:03:32 +0530

    Add files via upload

---
+++ Day 1/logger.js
@@ -0,0 +1,13 @@
+//console.log(__dirname)
+var url = 'https://www.google.com'
+function suriyaMovie(moviename){
+    console.log('Suriya')
+}
+
+function thalapathyMovie(moviename){
+    console.log('Joesph Vijay');
+}
+
+
+//module.exports = suriyaMovie('Sur')
+//module.exports = thalapathyMovie('Master')
