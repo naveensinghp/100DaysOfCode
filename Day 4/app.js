0f11dc588cf153f643ba20c44474cce4b46946ff
Naveen Singh <30367189+naveensinghp@users.noreply.github.com>
2021-04-08 10:30:35 +0530

    Day 7 Yargs argv

--- Day 4/app.js
+++ Day 4/app.js
@@ -1,5 +1,54 @@
 const validator = require('validator');
+const chalk = require("chalk");
+const yargs = require('yargs');
 
-console.log(validator)
 
-console.log('erri')
+yargs.version('1.1.0')
+
+yargs.command({
+    command:'add',
+    describe: 'Add a new note',
+    handler:function(){
+        console.log('Adding a new note');
+    }
+})
+
+
+yargs.command({
+    command:'remove',
+    describe: 'remove a note',
+    handler:function(){
+        console.log('Removing the note');
+    }
+})
+
+
+
+yargs.command({
+    command:'list',
+    describe: 'This is List Page',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption:true,
+            type:'string'
+        },
+        body :{
+            describe:'Note body',
+        }
+    },
+    handler:function(argv){
+        console.log('List Page',argv)
+    }
+})
+
+console.log(yargs.argv)
+
+
+// const test = chalk.green("Iam Green" + chalk.blue.underline.bold('With a blue suns'))
+// const red = chalk.blue.bgRed.bold('Shall We Begin')
+// console.log(red)
+
+
+
+
